400dc0aff1c2af579a458b7d807e508fd40daa44
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-03 17:14:56 -0500

    u

--- RTU/IMU/owa4x_AN8.c
+++ RTU/IMU/owa4x_AN8.c
@@ -169,11 +169,11 @@ int main( int argc, char *argv[] )
          printf("ERROR %d in getting MOVEMENT SENSOR\n", ReturnCode);
       } 
       else {
-         printf("{\"scale\": %d, \"x_axis\": %.3f, \"y_axis\": %.3f, \"z_axis\": %.3f}", MoveValue.scale, MoveValue.x_axis, MoveValue.y_axis, MoveValue.z_axis);
+         printf("{\"scale\": %d, \"x_axis\": %.3f, \"y_axis\": %.3f, \"z_axis\": %.3f}\n", MoveValue.scale, MoveValue.x_axis, MoveValue.y_axis, MoveValue.z_axis);
       }
 
       delay(100);
-      
+
    }
      
 
