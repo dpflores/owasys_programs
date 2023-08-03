03b2529ffb019767c9c94a08c25b34265a86694f
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-03 15:24:01 -0500

    u

--- GPS/Test_GPS_Module.c
+++ GPS/Test_GPS_Module.c
@@ -151,7 +151,7 @@ void GetFullGPSPosition( void )
 {
 	int   ReturnCode = 0;
 	tPOSITION_DATA LocalCoords;
-	static int x=0, NumOld = 0;
+	static int NumOld = 0;
 
    ReturnCode = GPS_GetAllPositionData( &LocalCoords );
    if( ReturnCode != NO_ERROR )
@@ -160,9 +160,8 @@ void GetFullGPSPosition( void )
 		if( LocalCoords.OldValue != 0){
 			NumOld++;
 		}
-		x++;
-		printf("\"cycles\": %d, \"pos_valid\": %hhu, \"old_pos\": %hhu, \"total\": %d, \"nav_status\": %s, \"lat\": %.7lf, \"lon\": %.7lf, \"alt\": %04.03f, \"hAcc\": %04.01f, \"vAcc\": %04.01f, \"Speed\": %04.03f, \"Course\": %04.02f, \"HDOP\": %04.03f, \"VDOP\": %04.01f, \"TDOP\": %04.01f, \"numSvs\": %hhu\n",
-            x, LocalCoords.PosValid, LocalCoords.OldValue, NumOld, LocalCoords.NavStatus, LocalCoords.LatDecimal, LocalCoords.LonDecimal,
+		printf("{\"pos_valid\": %hhu, \"old_pos\": %hhu, \"total\": %d, \"nav_status\": %s, \"lat\": %.7lf, \"lon\": %.7lf, \"alt\": %04.03f, \"hAcc\": %04.01f, \"vAcc\": %04.01f, \"Speed\": %04.03f, \"Course\": %04.02f, \"HDOP\": %04.03f, \"VDOP\": %04.01f, \"TDOP\": %04.01f, \"numSvs\": %hhu}\n",
+            LocalCoords.PosValid, LocalCoords.OldValue, NumOld, LocalCoords.NavStatus, LocalCoords.LatDecimal, LocalCoords.LonDecimal,
              LocalCoords.Altitude, LocalCoords.HorizAccu, LocalCoords.VertiAccu, LocalCoords.Speed,LocalCoords.Course, LocalCoords.HDOP,
              LocalCoords.VDOP, LocalCoords.TDOP, LocalCoords.numSvs);
 	
