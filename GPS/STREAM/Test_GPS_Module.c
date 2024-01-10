98953770c07fd6ace853fd4f1d46543e665de86e
acme <acme@acmecia.com>
2024-01-10 15:33:32 -0500

    U

--- GPS/STREAM/Test_GPS_Module.c
+++ GPS/STREAM/Test_GPS_Module.c
@@ -827,9 +827,9 @@ int main(int argc, char *argv[])
 
    
    while(1) {
-      
-      delay(1000);
-      GetFullGPSPosition();
+      ;
+      // delay(500);
+      // GetFullGPSPosition();
       
    }
 
